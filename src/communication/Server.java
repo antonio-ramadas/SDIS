1dbbeeec55bfe9964f3504b8b8d2077569eed66e
António Ramadas <up201303568@gcloud.fe.up.pt>
2016-03-06 18:44:52 +0000

    Created Server ID

--- src/communication/Server.java
+++ src/communication/Server.java
@@ -4,4 +4,16 @@ package communication;
  * Created by Antonio on 06-03-2016.
  */
 public class Server {
+    /**
+     * Server's id.
+     */
+    private static String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 }
