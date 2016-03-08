1a27211c33faad844b4fb623a661eddf489db7bd
António Ramadas <up201303568@gcloud.fe.up.pt>
2016-03-08 14:58:19 +0000

    Read file to bytes

--- src/source/Peer.java
+++ src/source/Peer.java
@@ -4,6 +4,11 @@ import communication.Server;
 import communication.Sockets;
 import storage.Backup;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by Antonio on 06-03-2016.
  */
@@ -14,6 +19,14 @@ public class Peer {
             System.out.println("Peer <server_id> <MC_IP> <MC_PORT> <MDB_IP> <MDB_PORT> <MDR_IP> <MDR_PORT>");
             return;
         }
+
+
+        /*Path path = Paths.get("chunks/caminho.txt");
+        try {
+            byte[] data = Files.readAllBytes(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
 
         Server.getInstance().setId(args[Arguments.SERVER_ID.ordinal()]);
         Server.getInstance().createChannel(Sockets.MULTICAST_CHANNEL,
