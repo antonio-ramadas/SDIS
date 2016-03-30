605df321204f9b04460642989ef35914b49e1153
Rui Vilares <rui.vilares@hotmail.com>
2016-03-30 22:38:18 +0100

    cli done!!!!

--- src/source/Peer.java
+++ src/source/Peer.java
@@ -12,18 +12,152 @@ import storage.Backup;
 import storage.Chunk;
 
 import java.io.*;
+import java.net.*;
+import console.MessageCenter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 /**
  * Created by Antonio on 06-03-2016.
  */
 public class Peer {
 
-    public static void main(String args[]) {
+    private static int port;
+
+    public static void main(String args[]) throws IOException {
+
+        if (!validateArgs(args))
+            return;
+
+        ServerSocket srvSocket = null;
+        Socket socket = null;
+
+        try{
+            srvSocket = new ServerSocket(port);
+        } catch (IOException e){
+            MessageCenter.output("Could not listen on port: " + port);
+            System.exit(-1);
+        }
+
+        boolean end = false;
+        while(!end){
+
+            try {
+                socket = srvSocket.accept();
+            } catch (IOException e) {
+                MessageCenter.error("Accept failed: " + port);
+                System.exit(1);
+            }
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            String request = in.readLine();
+
+            String[] splitRequest = request.split(" ");
+
+
+            String sub_protocol = splitRequest[0];
+            String response;
+
+            if (sub_protocol.equals("BACKUP")){
+                String opnd_1 = splitRequest[1];
+                int opnd_2 = Integer.parseInt(splitRequest[2]);
+                response = backup(opnd_1, opnd_2);
+            }
+            else if (sub_protocol.equals("RESTORE")){
+                String opnd_1 = splitRequest[1];
+                response = restore(opnd_1);
+            }
+            else if (sub_protocol.equals("DELETE")){
+                String opnd_1 = splitRequest[1];
+                response = delete(opnd_1);
+            }
+            else if (sub_protocol.equals("SPACE")){
+                int opnd_1 = Integer.parseInt(splitRequest[1]);
+                response = space(opnd_1);
+            }
+            else {
+                response = "Without response";
+            }
+
+            out.println(response);
+            MessageCenter.output(response);
+
+            out.close();
+            in.close();
+
+            socket.close();
+        }
+        srvSocket.close();
+    }
+
+    private static boolean validateArgs(String[] args){
+        if (args.length != 1){
+            MessageCenter.output("Usage: java Server <port>");
+            return false;
+        }
+        else {
+            port = Integer.parseInt(args[0]);
+            MessageCenter.output("Port: " + port);
+            return true;
+        }
+    }
+
+    private static String backup(String filePath, int replicationDegree){
+        return "backup " + filePath + " " + replicationDegree;
+    }
+
+    private static String delete(String filePath){
+        return "delete " + filePath;
+    }
+
+    private static String restore(String filePath){
+        return "restore " + filePath;
+    }
+
+    private static String space(int amountOfSpace){
+        return "space " + amountOfSpace;
+    }
+
+    // Corrigir
+    public static void splitFile(File f) throws IOException {
+        int partCounter = 1;
+
+        int sizeOfFiles = 64 * 1024;// 64 Kb
+        byte[] buffer = new byte[sizeOfFiles];
+
+        try (BufferedInputStream bis = new BufferedInputStream(
+                new FileInputStream(f))) {//try-with-resources to ensure closing stream
+            String name = f.getName();
+
+            int tmp = 0;
+            while ((tmp = bis.read(buffer)) > 0) {
+                //write each chunk of data into separate file with different number in name
+                File newFile = new File(f.getParent(), name + "."
+                        + String.format("%03d", partCounter++));
+                try (FileOutputStream out = new FileOutputStream(newFile)) {
+                    out.write(buffer, 0, tmp);//tmp is chunk size
+                }
+            }
+        }
+    }
+
+    // Corrigir esta parte
+    public static void mergeFiles(List<File> files, File into)
+            throws IOException {
+        try (BufferedOutputStream mergingStream = new BufferedOutputStream(
+                new FileOutputStream(into))) {
+            for (File f : files) {
+                Files.copy(f.toPath(), mergingStream);
+            }
+        }
+    }
+
+        /*
         if (args.length != Arguments.NUMBER_ALLOWED_OF_ARGUMENTS.ordinal()) {
             System.out.println("Peer <server_id> <MC_IP> <MC_PORT> <MDB_IP> <MDB_PORT> <MDR_IP> <MDR_PORT>");
             return;
@@ -102,5 +236,5 @@ public class Peer {
         //não faz mais nada
         //método de teste
         Backup.getInstance();
-    }
+    }*/
 }
--- src/test/TestApp.java
+++ src/test/TestApp.java
@@ -2,6 +2,7 @@ package test;
 
 import java.io.*;
 import java.net.*;
+import console.MessageCenter;
 
 public class TestApp {
 
@@ -31,7 +32,7 @@ public class TestApp {
         out.println(request);
         String response = in.readLine();
 
-        System.out.println(response);
+        MessageCenter.output(response);
 
         out.close();
         in.close();
@@ -41,53 +42,94 @@ public class TestApp {
 
     private static boolean validateArgs(String[] args) {
         if (args.length < 3 || args.length > 4){
-            System.out.println("Usage: java TestApp <peer_ap> <sub_protocol> <opnd_1> <opnd_2>");
+            MessageCenter.output("Usage: java TestApp <peer_ap> <sub_protocol> <opnd_1> <opnd_2>");
             return false;
         }
         else{
             peer_ap = args[0];
             String[] peer_ap_split = peer_ap.split(":");
             address = peer_ap_split[0];
+            if (!validStringToInt(peer_ap_split[1])){
+                return false;
+            }
             port = Integer.parseInt(peer_ap_split[1]);
 
-            sub_protocol = args[2];
+            sub_protocol = args[1];
 
             if (sub_protocol.equals("BACKUP")){
                 if (args.length != 4) {
-                    System.out.println("Usage: java TestApp <address:port> BACKUP <file path> <replication degree>");
+                    MessageCenter.output("Usage: java TestApp <address:port> BACKUP <file path> <replication degree>");
+                    return false;
+                }
+                if (!validFilePath(args[2])) {
                     return false;
                 }
                 opnd_1 = args[2];
+                if (!validStringToInt(args[3])){
+                    return false;
+                }
                 opnd_2 = args[3];
             }
             else if (sub_protocol.equals("RESTORE")){
                 if (args.length != 3) {
-                    System.out.println("Usage: java TestApp <address:port> RESTORE <file path>");
+                    MessageCenter.output("Usage: java TestApp <address:port> RESTORE <file path>");
+                    return false;
+                }
+                if (!validFilePath(args[2])) {
                     return false;
                 }
                 opnd_1 = args[2];
             }
             else if (sub_protocol.equals("DELETE")){
                 if (args.length != 3) {
-                    System.out.println("Usage: java TestApp <address:port> DELETE <file path>");
+                    MessageCenter.output("Usage: java TestApp <address:port> DELETE <file path>");
+                    return false;
+                }
+                if (!validFilePath(args[2])) {
                     return false;
                 }
                 opnd_1 = args[2];
             }
             else if (sub_protocol.equals("SPACE")){
                 if (args.length != 3) {
-                    System.out.println("Usage: java TestApp <address:port> SPACE <amount of space>");
+                    MessageCenter.output("Usage: java TestApp <address:port> SPACE <amount of space>");
+                    return false;
+                }
+                if (!validStringToInt(args[2])){
                     return false;
                 }
                 opnd_1 = args[2];
             }
 
             else{
-                System.out.println("Sub Protocols: BACKUP, RESTORE, DELETE, SPACE");
+                MessageCenter.output("Sub Protocols: BACKUP, RESTORE, DELETE, SPACE");
                 return false;
             }
 
 
+        }
+        return true;
+    }
+
+    private static boolean validFilePath(String path) {
+        File file = new File(path);
+        if (!file.exists()) {
+            MessageCenter.output(file.getAbsolutePath() + " does not exist");
+            return false;
+        } else if (!file.isFile()) {
+            MessageCenter.output(file.getAbsolutePath() + " is not a file, it is a folder");
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean validStringToInt(String intStr) {
+        try {
+            Integer.parseInt(intStr);
+        } catch (NumberFormatException e) {
+            MessageCenter.error(intStr + " must be a valid integer");
+            e.printStackTrace();
+            return false;
         }
         return true;
     }
